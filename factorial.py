28c416ea73bb3af7fdc281678545744aa5e42e53
David Hatch <hozer48@gmail.com>
2015-11-03 14:27:24 -0800

    Adds timer and removes print statements.

--- factorial.py
+++ factorial.py
@@ -2,26 +2,28 @@
 """ Examples taken from: http://www.python-course.eu/recursive_functions.php """
 
 import pdb
+import time
+start = time.time()
 
 """ Recursive function for calculating factorials """
 def factorial(n):
-    print("factorial has been called with n = " + str(n))
     if n == 1:
         return 1
     else:
         res = n * factorial(n-1)
-        print("intermediate result for ", n, " * factorial(" ,n-1, "): ",res)
         return res	
 
 #pdb.run('print(factorial(4))')
-print(factorial(5))
+print(factorial(100))
 
 
 """ Iterative function for calculating factorials """
 def iterative_factorial(n):
     result = 1
     for i in range(2,n+1):
-        print("i: ", i, "result: ", result)
         result *= i
     return result
-#print(iterative_factorial(5))
+#print(iterative_factorial(100))
+
+end = time.time()
+print(end - start)
