d0f466e0465a56f29d2b78c3fd899d801591e84f
David Hatch <hozer48@gmail.com>
2016-03-09 14:03:29 -0800

    Adds my selenium bing test. Also adds a wordlist and shell script to update the selenium test from the wordlist.

---
+++ update_selenium_search.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+wordListFile="$HOME/bin/wordListClean.txt"
+scriptFile="$HOME/bin/selenium_test_30.html"
+
+# Get index from 1st line of file and then increment
+read -r index < "$wordListFile"
+(( index++ ))
+
+# Loop the word list file
+lineCount=$(wc -l "$wordListFile")
+if [[ $index > $lineCount ]];then
+    index=2
+fi
+
+# Get the new word from that line index
+newWord="$(sed -n ''$index' p' <"$wordListFile")"
+
+# Replace script with new search word and update index in wordlist
+sed -i.bak "27 s/>.*</>$newWord</" "$scriptFile" && sed -i.bak '1 s/^.*$/'$index'/' "$wordListFile"
