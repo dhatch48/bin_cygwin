41fadde7aa8861bbfde12bdc36d7fc7de38ab713
David Hatch <hozer48@gmail.com>
2018-11-08 13:11:27 -0800

    get_user_enabled - Adds column SamAccountName to show login name. Sorts by name.

--- windows/get_user_enabled.ps1
+++ windows/get_user_enabled.ps1
@@ -1,4 +1,4 @@
 ﻿Get-ADUser -filter {Enabled -eq $True} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed", "PasswordLastSet" |
-Select-Object -Property "Displayname","PasswordLastSet",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} |
-sort ExpiryDate |
+Select-Object -Property "SamAccountName", "Displayname","PasswordLastSet",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} |
+sort SamAccountName |
 Export-Csv -NoTypeInformation -Delimiter "`t" D:\Users\david\Documents\user_list.txt
