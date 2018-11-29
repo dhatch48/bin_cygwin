01f1549c3e9af6bd8a4e98617ace7b3a94d18ad8
David Hatch <hozer48@gmail.com>
2018-11-29 12:49:04 -0800

    printProj - Adds script to print a given KMI project for Patrick. This requires pdftoprinter.exe be installed and in PATH.

---
+++ printProj.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -eo pipefail    # Turns on strict error options - exit on first error.
+
+#/ Usage: printProj.sh [OPTION] FILE
+#/ Description:
+#/ Examples: printProj.sh PROJECTNAME
+#/ Options:
+#/      -h: Display this help message
+#/      -d: debug mode
+#/      $1: project name
+
+# Basic helpers
+out() { echo "$(date '+%Y-%m-%d %H:%M:%S') $@"; }
+err() { out "[ERROR] $@" 1>&2; echo; }
+die() { out "[FATAL] $1" && [ "$2" ] && [ "$2" -ge 0 ] && exit "$2" || exit 1; }
+usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
+
+# Parse options
+while getopts "dh" o; do
+    case $0 in
+        d) DEBUG=1 ;;
+        h) usage ;;
+    esac
+done
+shift $((OPTIND-1))
+
+[ "$DEBUG" ] && set -x
+# Script start
+###############################################################################
+
+[ -z "$1" ] && usage
+
+myprint=/cygdrive/d/Temp/print/
+cd /cygdrive/h/PROJECTS/KMI/"$1"/
+
+### Clean temp print folder
+rm -f "$myprint"*
+
+### Filter out duplicates and granite trim files
+cp "$1.PDF" "$myprint"
+cp -n KMI-*/*.PDF EXTRA-SHEETMETAL/*.PDF "$myprint"
+
+### Print the files
+for file in $myprint/*; do
+    #echo "Printing: $(cygpath -aw $file)"
+    pdftoprinter.exe "$(cygpath -aw $file)" "Copier_2nd_Floor_PCL"
+done
