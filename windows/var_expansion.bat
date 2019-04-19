c46c415da7cbe65dc5b4d467755c2db2cf5b6a27
U-SAFETY\dhatc <dhatc@LT6314.safety.ci.carlsbad.ca.us>
2019-04-19 16:08:43 -0700

    var_expansion.bat - Adds an example variable expansion.

---
+++ windows/var_expansion.bat
@@ -0,0 +1,18 @@
+@echo off
+
+REM The %~dp0 (that’s a zero) variable when referenced within a Windows batch file will expand to the drive letter and path of that batch file.
+
+REM The variables %0-%9 refer to the command line parameters of the batch file. %1-%9 refer to command line arguments after the batch file name. %0 refers to the batch file itself.
+
+REM If you follow the percent character (%) with a tilde character (~), you can insert a modifier(s) before the parameter number to alter the way the variable is expanded. The d modifier expands to the drive letter and the p modifier expands to the path of the parameter.
+
+echo Full Path       : %~f0
+echo Drive + path    : %~dp0
+echo File name + ext : %~nx0
+echo File short      : %~fs0
+
+echo Var1 DP         : %~dp1
+echo Var2 DP         : %~dp2
+
+
+
