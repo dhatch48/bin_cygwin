47dbf2f6fb5a53a6ac5783a05ca3d65ae1beb3a9
U-SAFETY\dhatc <dhatc@LT6314.safety.ci.carlsbad.ca.us>
2019-04-23 14:10:52 -0700

    robocopy_ams.bat - Adds common option reference and removes duplicate examples.

--- windows/robocopy_ams.bat
+++ windows/robocopy_ams.bat
@@ -1,8 +1,9 @@
 @echo off
 REM https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-R2-and-2012/cc733145(v=ws.11)
 
-REM Excludes different size, newer files and older files
-::robocopy \\vm5\web\ams d:\wamp\www\ams /E /XC /XN /XO
+REM /XO Excludes older files
+REM /XN Excludes newer files
+REM /XC Excludes different size
 
 REM Excludes different size and older files
 ::robocopy \\vm5\web\ams d:\wamp\www\ams /E /XC /XO
