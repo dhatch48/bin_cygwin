b8f4d23f9b05e0b3081f1bb799ab7a22fd329ea2
U-SAFETY\dhatc <3mail48@gmail.com>
2019-04-25 23:27:06 -0700

    var_expansion.bat - Adds web reference. Adds plain var0.

--- windows/var_expansion.bat
+++ windows/var_expansion.bat
@@ -1,11 +1,14 @@
 @echo off
 
+REM https://stackoverflow.com/questions/5034076/what-does-dp0-mean-and-how-does-it-work
+
 REM The %~dp0 (that’s a zero) variable when referenced within a Windows batch file will expand to the drive letter and path of that batch file.
 
 REM The variables %0-%9 refer to the command line parameters of the batch file. %1-%9 refer to command line arguments after the batch file name. %0 refers to the batch file itself.
 
 REM If you follow the percent character (%) with a tilde character (~), you can insert a modifier(s) before the parameter number to alter the way the variable is expanded. The d modifier expands to the drive letter and the p modifier expands to the path of the parameter.
 
+echo Var0            : %0
 echo Full Path       : %~f0
 echo Drive + path    : %~dp0
 echo File name + ext : %~nx0
@@ -14,6 +17,3 @@ echo File short      : %~fs0
 echo Var1            : %1
 echo Var1 Full Path  : %~f1
 echo Var2 Full Path  : %~f2
-
-
-
