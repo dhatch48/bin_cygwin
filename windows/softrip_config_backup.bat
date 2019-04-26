2884c886c707f26cb8f04bdbed65ef89ead26335
David <hozer48@gmail.com>
2019-04-26 13:55:20 -0700

    softrip_config_backup - no change - changes uppercase to lower.

--- windows/softrip_config_backup.bat
+++ windows/softrip_config_backup.bat
@@ -1,5 +1,5 @@
-@ECHO OFF
-SETLOCAL
+@echo off
+setlocal
 
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 	set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
@@ -7,25 +7,25 @@ for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "d
 
 set "datestamp=%YYYY%-%MM%-%DD%"
 
-SET _source=c:\wwrip70\Configurations\epsonsct3200
+set _source=c:\wwrip70\Configurations\epsonsct3200
 
-SET _dest=c:\backup\%datestamp%
+set _dest=c:\backup\%datestamp%
 
-SET _what=/S /COPYALL
+set _what=/S /COPYALL
 :: /COPYALL :: COPY ALL file info
 :: /B :: copy files in Backup mode.
 :: /MIR :: MIRror a directory tree
 
-SET _options=/R:0 /W:0 /LOG+:C:\backup\RoboLog.txt /NFL /NDL
+set _options=/R:0 /W:0 /LOG+:C:\backup\RoboLog.txt /NFL /NDL
 :: /R:n :: number of Retries
 :: /W:n :: Wait time between retries
 :: /LOG :: Output log file
 :: /NFL :: No file logging
 :: /NDL :: No dir logging
 
-ROBOCOPY %_source% %_dest%\Configurations\epsonsct3200 %_what% %_options%
+robocopy %_source% %_dest%\Configurations\epsonsct3200 %_what% %_options%
 
-ROBOCOPY c:\hf_printer-1 %_dest%\hf_printer-1 %_what% %_options% *.cfg
-ROBOCOPY c:\hf_printer-2 %_dest%\hf_printer-2 %_what% %_options% *.cfg
-ROBOCOPY c:\hf_printer-3 %_dest%\hf_printer-3 %_what% %_options% *.cfg
-ROBOCOPY c:\hf_printer-4 %_dest%\hf_printer-4 %_what% %_options% *.cfg
+robocopy c:\hf_printer-1 %_dest%\hf_printer-1 %_what% %_options% *.cfg
+robocopy c:\hf_printer-2 %_dest%\hf_printer-2 %_what% %_options% *.cfg
+robocopy c:\hf_printer-3 %_dest%\hf_printer-3 %_what% %_options% *.cfg
+robocopy c:\hf_printer-4 %_dest%\hf_printer-4 %_what% %_options% *.cfg
