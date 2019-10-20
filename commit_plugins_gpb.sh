49d835a7017a0d6f0ea7fff877db61921aae3fb6
David <hozer48@gmail.com>
2019-10-19 22:00:59 -0700

    commit_plugins_gpb - Adds script to batch git commit WordPress plugin changes.

---
+++ commit_plugins_gpb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#set -x
+
+shopt -s expand_aliases
+alias wp=~/bin/wp-cli.phar
+
+pluginDir="$HOME/public_html/wp-content/plugins"
+inputFile=${1:-$HOME/bin/plugin.csv}
+
+while IFS= read -r line; do
+    pluginName=$(awk -F, '{print $1}' <<< "$line")
+    oldVersion=$(awk -F, '{print $2}' <<< "$line")
+    newVersion=$(awk -F, '{print $3}' <<< "$line")
+    pluginTitle=$(cd "$pluginDir"; wp plugin get $pluginName --fields=title --format=csv | tail -1 | cut -d',' -f2)
+
+    cd "$pluginDir"
+    git add "./$pluginName"
+    git commit -m "Plugin - Updates $pluginTitle from version $oldVersion => $newVersion" >/dev/null; status=$?
+    if [[ $status == 0 ]];then
+        echo "Success: $pluginName"
+    else
+        echo "Error: Commit failed for $pluginName"
+    fi
+
+done < "$inputFile"
