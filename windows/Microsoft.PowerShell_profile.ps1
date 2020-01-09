c1a73562170626dcc7bf164505f4f32582913aec
U-SAFETY\dhatc <3mail48@gmail.com>
2020-01-08 17:38:31 -0800

    PowerShell_profile - Adds new shortcut functions for net user and get-adcomputer.

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -25,6 +25,15 @@ function down {
 	cd $HOME\Downloads
 }
 
+function nu {
+    net user /domain $args
+}
+
+function find-adcomputer {
+    get-adcomputer -Filter "description -like `"*$args*`"" -Properties description,IPv4Address
+}
+new-alias fac find-adcomputer
+
 # Welcome
 "Welcome {0}, to Powershell {1}" -f $ENV:username,($PSVersionTable.PSVersion)
 "Today is {0:F}`n" -f (get-date)
