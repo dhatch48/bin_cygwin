31a132c1cd589e0c18c736c209935589ee916ead
dhatc <3mail48@gmail.com>
2021-02-10 12:21:19 -0800

    find_folder_in_appdata.ps1 - Adds comment header.

--- windows/find_folder_in_appdata.ps1
+++ windows/find_folder_in_appdata.ps1
@@ -1,3 +1,9 @@
+<#
+.SYNOPSIS
+    Delete leftover Adobe Flash Player folder from each users' appdata
+.NOTES
+    Requires admin privilege
+#>
 foreach ($d in $(Get-ChildItem C:\Users\ -Directory)) {
     Get-ChildItem "$($d.FullName)\AppData\Roaming" -Directory -Recurse -Filter "Flash Player" -ErrorAction SilentlyContinue | Select-Object -Property FullName
     #Get-ChildItem "$($d.FullName)\AppData\Roaming" -Directory -Recurse -Filter "Flash Player" -ErrorAction SilentlyContinue | Remove-Item -Recurse
