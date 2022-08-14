6cc584fa53454f3a22a39932d848da2f9f465785
David <hozer48@gmail.com>
2022-08-13 20:43:45 -0700

    net_test.ps1 - Speeds up execution time. Changes from ping to PS test-connection with 2 attempts instead of 4.

--- windows/net_test.ps1
+++ windows/net_test.ps1
@@ -4,11 +4,11 @@ $netAdapterIndex = Get-NetAdapter | Where-Object {$_.Status -eq "Up"} | select i
 
 $defaultGateway =  Get-NetRoute | Where-Object {$_.DestinationPrefix -eq '0.0.0.0/0'} | Select { $_.NextHop } -ExpandProperty NextHop
 
-ping $defaultGateway
-ping google.com
+Test-Connection $defaultGateway -Count 2 | ft
+Test-Connection google.com -Count 2 | ft
 
 Write-Host
-if ((Test-Connection $defaultGateway -Count 2 -Quiet) -And (Test-Connection google.com -Count 2 -Quiet)) {
+if ((Test-Connection $defaultGateway -Count 1 -Quiet) -And (Test-Connection google.com -Count 1 -Quiet)) {
     Write-Host "Connection test to gateway and google passed"
 } Else {
     Write-Host "Connection test failed"
