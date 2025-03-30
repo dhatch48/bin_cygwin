1961a303bc617e8f4a5dfd4734e76ebc5ef8c6eb
David <hozer48@gmail.com>
2025-03-29 22:14:13 -0700

    pdf_bate_number.py - Changes output to print/log all files. Result file has pattern in name now. Changes CSV output format with different column order and includes new column for file extension.

--- windows/pdf_bate_number.py
+++ windows/pdf_bate_number.py
@@ -16,26 +16,31 @@ change dirPath string below or default is current script path
 '''
 dirPath = os.getcwd()
 
-# define bate number pattern
+# define bate number pattern and resultFile
 search_pattern = '[A-Z]{2,}[-_]?[0-9]{4,10}+'
+resultFile = (os.getcwd() + "\\pdf_bates_number_results.csv")
 
 # set string prefix if passed
 if len(sys.argv) == 2:
     searchPrefix = str(sys.argv[1])
     search_pattern = searchPrefix + '[0-9]{4,10}+'
+    resultFile = (os.getcwd() + "\\pdf_bates_number_" + searchPrefix + "results.csv")
 
 # save results to CSV file in the same folder as this script
-resultFile = (os.getcwd() + "\\pdf_bate_number_results.csv")
-writer = csv.writer(open(resultFile, 'w', newline='', encoding='utf-8'))
+writer = csv.writer(open(resultFile, 'w', newline='', encoding='utf-8'), dialect='excel')
 # write header
-writer.writerow(["file_path","filename","bate_number","page_count"])
+writer.writerow(["Bates number", "Page count" ,"File path" ,"File name", "File extension", "Native path"])
 
-# loop recursively through all pdf files
+# loop recursively through all folders and files
 for (path, directories, files) in os.walk(dirPath):
     for curFile in files:
-        if curFile.casefold().endswith('pdf'):
-            fullFilePath = os.path.join(path,curFile)
-            relFilePath = os.path.relpath(fullFilePath,dirPath)
+        # get current file extension and file path
+        fileExt = os.path.splitext(curFile)[1][1:].strip().lower()
+        fullFilePath = os.path.join(path,curFile)
+        relFilePath = os.path.relpath(fullFilePath,dirPath)
+
+        # Search only PDF files
+        if fileExt == "pdf":
 
             # open the pdf file
             #reader = PdfReader(fullFileName)
@@ -52,12 +57,22 @@ for (path, directories, files) in os.walk(dirPath):
 
             # write each result to CSV file
             if res_search:
-                row = (relFilePath, curFile,res_search.group(0),pageCount)
+                row = (res_search.group(0), pageCount, relFilePath, curFile, fileExt, curFile)
             else:
-                row = (relFilePath, curFile,"no match found",pageCount)
-            
+                row = ("no match found", pageCount, relFilePath, curFile, fileExt, curFile)
             print(row)
             writer.writerow(row)
+        
+        # Ignore this script and result file
+        elif curFile.startswith("pdf_bate"):
+            exit
+
+        # log all other files
+        else:
+            row = ("n/a", "n/a", relFilePath, curFile, fileExt, curFile)
+            print(row)
+            writer.writerow(row)
+
 print("results saved to: ", '\033[1m' + resultFile + '\033[0m')
 print("done!")
 
